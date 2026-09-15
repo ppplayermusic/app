a77349b8f8c520527ffa30034eca8c16e00fbfba
Lucas Coelho de Oliveira Lima <lucas_coelhoveneno@outlook.com>
2026-09-15 10:57:23 -0400

    refactor: format test_taskbar.dart and add avoid_print ignore rule

--- test_taskbar.dart
+++ test_taskbar.dart
@@ -1,1 +1,6 @@
-import 'package:windows_taskbar/windows_taskbar.dart'; void main() { print(ThumbnailToolbarButton); }
+// ignore_for_file: avoid_print
+import 'package:windows_taskbar/windows_taskbar.dart';
+
+void main() {
+  print(ThumbnailToolbarButton);
+}
